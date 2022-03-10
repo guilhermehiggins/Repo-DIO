7814a0b7769c152cf91e0033895a67c5a14536b8
guilhermehiggins <guilherme.higgins@hotmail.com>
2022-03-10 18:28:56 -0300

    Adicionando os desafios 5 e 6

---
+++ Desafios/Desafio 4/Desafio4/Desafio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, quociente;
+            int[] notas = new int[] { 100, 50, 20, 10, 5, 2, 1 };
+
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n);
+
+            foreach (var nota in notas)
+            {
+                quociente = n / nota;
+                Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+                n = n % nota;
+            }
+        }
+    }
+}
---
+++ Desafios/Desafio 5/Desafio5/Desafio5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double a, b, c;
+            string[] valor = Console.ReadLine().Split();
+            a = Convert.ToDouble(valor[0], CI);
+            b = Convert.ToDouble(valor[1], CI);
+            c = Convert.ToDouble(valor[2], CI);
+
+            double soma = a + b;
+
+            if((a < b + c) && (b < a + c) && (c < b + a))
+            {
+                double perimetro = a + b + c;
+                Console.WriteLine($"Perimetro = {perimetro.ToString("F1", CI)}");
+            }
+            else
+            {
+                double area = ((a + b) * c) / 2.0;
+                Console.WriteLine($"Area = {area.ToString("F1", CI)}");
+            }
+
+        }
+    }
+}
